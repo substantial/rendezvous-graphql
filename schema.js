fa95ee4ef2387e88d7d713cf0882cb2a3df9fb02
Shawn Dempsey <shwanton@gmail.com>
2018-10-04 22:47:30 -0700

    Add album root and artist resolver

--- schema.js
+++ schema.js
@@ -11,10 +11,27 @@ const ArtistType = new GraphQLObjectType({
   }
 });
 
+const AlbumType = new GraphQLObjectType({
+  name: "Album",
+  fields: {
+    id: { type: GraphQLString },
+    title: { type: GraphQLString },
+    artist: {
+      type: ArtistType,
+      resolve(parentValue, args) {
+        console.log(parentValue, args);
+        return axios
+          .get(`http://localhost:3000/artists/${parentValue.artistId}`)
+          .then(response => response.data);
+      }
+    }
+  }
+});
+
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
-    Artist: {
+    artist: {
       type: ArtistType,
       args: {
         id: { type: GraphQLString }
@@ -22,6 +39,17 @@ const RootQuery = new GraphQLObjectType({
       resolve(parentValue, args) {
         return axios
           .get(`http://localhost:3000/artists/${args.id}`)
+          .then(response => response.data);
+      }
+    },
+    album: {
+      type: AlbumType,
+      args: {
+        id: { type: GraphQLString }
+      },
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/albums/${args.id}`)
           .then(response => response.data);
       }
     }
